c951fc0840b9baf342e0b445a72a30b3435c40c6
Johnny Lu <Johnny.Lu@acronis.com>
2022-10-11 08:11:17 +0000

    update setup.py add example notebook in chatbot sub directory complete load_dataset.py add README.md for dataset subdirectory

--- sentiment/dataset/load_dataset.py
+++ sentiment/dataset/load_dataset.py
@@ -6,6 +6,7 @@ from pymongo import MongoClient
 import pandas as pd
 from dotenv import load_dotenv
 import os
+from pprint import pformat
 
 load_dotenv()
 
@@ -22,7 +23,7 @@ class LoadDataset:
 
     def __init__(self, database_name: str, 
                  collection_name:str,
-                 n_rows: int) -> None:
+                 n_rows: int = 10000) -> None:
         self.client = MongoClient(
             MONGO_URL,
             username=PYMONGO_USERNAME,
@@ -32,15 +33,47 @@ class LoadDataset:
         self.collection = self.db[collection_name]
         self.max = n_rows
     
-    # def __next__(self):
-    #     cursor = self.collection.find()
-    #     for item in cursor:
-    #         if self.n <= self.max:
-    #             return item
-    #         else:
-    #             raise StopIteration
+    def __len__(self) -> int:
+        return self.collection.count_documents({})
     
-    # def __iter__(self):
-    #     self.n = 0
-    #     return self
+    def __getitem__(self, index):
+        return self.collection.find()[index]
 
+    def __repr__(self) -> str:
+        sample = self.collection.find()[0]
+        return f"""
+        Database: {str(self.db)},
+        Collection: {str(self.collection)}
+        Length : {self.collection.count_documents({})}
+        Sample: {pformat(sample)}
+        """
+
+    def __iter__(self):
+        for document in self.collection.find():
+            yield document
+
+    def to_pandas(self):
+        """
+        Iterate through the whole db collection, transform it into Pandas DataFrame and return
+        """
+        cursor = self.collection.find()
+        return pd.DataFrame(list(cursor))
+    
+    def tokenize(self):
+        raise NotImplementedError
+
+    def update_one(self):
+        raise NotImplementedError
+    
+    def update_many(self):
+        raise NotImplementedError
+    
+
+
+
+if __name__ == "__main__":
+    dataset = LoadDataset(
+        database_name="PLP",
+        collection_name="tweet"
+    )
+    print(dataset)
--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     install_requires=[
         "pymongo==3.12.0",
         "numpy==1.23.1",
-        "pandas==1.4.4"
+        "pandas==1.4.4",
+        "snscrape"
     ]
 )
