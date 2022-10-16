0e60fa243c6c33f70fc48a53c4e77b72466075a1
Johnny Lu <Johnny.Lu@acronis.com>
2022-10-16 23:06:29 +0800

    Johnny code update - update load_dataset.py for adding column names - update setup.py for proper dependency - add transformer_model.py

---
+++ sentiment/model/transformer_model.py
@@ -0,0 +1,8 @@
+"""
+Author: Lu.ZhiPing
+Email: lu.zhiping@u.nus.edu
+"""
+from sentiment.dataset.load_dataset import LoadDataset
+from datasets import load_dataset
+from transformers import AutoTokenizer
+from transformers import AutoModelForSequenceClassification
